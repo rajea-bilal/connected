eebfab01fc6278b43e1f28301dcb2a42ae2bab90
rajea-bilal <rajea.bilal@gmail.com>
2022-10-26 17:42:16 +0100

    added logout button to the bottom & added a welcome message on profile page

--- routes/posts.js
+++ routes/posts.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const commentsController = require("../controllers/comments");
+const upload = require("../middleware/multer");
+const postsController = require("../controllers/posts");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 
 
 //Post Routes - simplified for now
+router.get("/:id", ensureAuth, postsController.getPost);
+
+router.post("/createPost", upload.single("file"), postsController.createPost);
 
 
-router.post("/createComment/:id", commentsController.createComment);
+router.put("/likePost/:id", postsController.likePost);
 
-router.put("/likeComment/:postid/:commentid", commentsController.likeComment);
-
-router.delete("/deleteComment/:postid/:commentid", commentsController.deleteComment);
+router.delete("/deletePost/:id", postsController.deletePost);
 
 module.exports = router;
